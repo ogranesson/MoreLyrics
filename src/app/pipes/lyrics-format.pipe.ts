68ece731d3bbb24749f155e9b2f196d9a59c2509
Anshu Shrestha <r0927058@student.thomasmore.be>
2024-05-27 22:06:02 +0200

    dfdfd

--- src/app/pipes/lyrics-format.pipe.ts
+++ src/app/pipes/lyrics-format.pipe.ts
@@ -8,7 +8,6 @@ export class LyricsFormatPipe implements PipeTransform {
 
   transform(lyrics: string): string {
     return lyrics.replace(/\n/g, '<br>')
-                  .replace(/\b[A-G][#b\d]?(m|maj|min|aug|dim|sus(2|4)?|add)?\d*(\/[A-G][#b\d]?)?\b/g, '<span class="text-green-900 bg-green-200 font-semibold p-0.5">$&</span>');
+                  .replace(/\b[A-G][#b\d]?(m|maj|min|aug|dim|sus(2|4)?|add)?\d*(\/[A-G][#b]?)?\b/gm, '<span class="text-green-900 bg-green-200 font-semibold p-0.5">$&</span>');
   }
-
 }
