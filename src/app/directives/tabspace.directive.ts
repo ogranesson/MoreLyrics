ae6f210fe903a0f27c01651935ab167becb0a9cb
ogranesson <shresthaanshu555@gmail.com>
2024-10-22 23:26:15 +0200

    Adding comments, removing title bar

--- src/app/directives/tabspace.directive.ts
+++ src/app/directives/tabspace.directive.ts
@@ -1,25 +1,28 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appTabspace]',
   standalone: true
 })
 export class TabspaceDirective {
-  @HostListener('keydown', ['$event'])
+  @HostListener('keydown', ['$event']) // required for listening to events
   handleKeyDown(event: KeyboardEvent) {
     const inputElement = event.target as HTMLInputElement | HTMLTextAreaElement;
 
     if (event.key === 'Tab') {
       event.preventDefault();
       const start = inputElement.selectionStart;
-      const end = inputElement.selectionEnd;
+      const end = inputElement.selectionEnd; // when multiple characters not selected, these values are the same
 
       if (start === null || end === null) {
         console.error("Cursor position not available");
         return;
       }
 
+      // substring(0, start) gets text before the cursor position, adds tabspace, and substring(end) gets the rest of the text
       inputElement.value = inputElement.value.substring(0, start) + '\t' + inputElement.value.substring(end);
+
+      // since start + 1 is the tabspace, this puts the cursor after the tabspace
       inputElement.selectionStart = inputElement.selectionEnd = start + 1;
     }
   }
