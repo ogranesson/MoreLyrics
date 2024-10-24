a26329496b31432a62d6379417ba0c444760b832
ogranesson <shresthaanshu555@gmail.com>
2024-10-24 08:57:16 +0200

    Chord regex fix

--- src/app/pipes/lyrics-format.pipe.ts
+++ src/app/pipes/lyrics-format.pipe.ts
@@ -10,6 +10,6 @@ export class LyricsFormatPipe implements PipeTransform {
   transform(lyrics: string): string {
     return lyrics.replace(/\\t/g, '&nbsp;&nbsp;&nbsp;&nbsp;')
                   .replace(/\\n/g, '<br>')
-                  .replace(/\b[A-G][\#b\d]?(m|maj|min|aug|dim|sus(2|4)?|add)?\d*(\/[A-G][\#b]?)?/gm, '<span class="text-green-900 bg-green-200 font-semibold p-0.5">$&</span>');
+                  .replace(/\b([A-G](b|#|bb)?)(maj|min|m|M|\+|-|dim|aug)?[0-9]*(sus[24]?)?(\/[A-G][b|bb]?)?\b(#)?/gm, '<span class="text-green-900 bg-green-200 font-semibold p-0.5">$&</span>');
   }
 }
