091c494ea1e5f3dec0bf5ebdc676a3aacfe3c5a7
ogranesson <shresthaanshu555@gmail.com>
2024-10-31 14:01:04 +0100

    Fix routing with guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { loggedInGuard } from './guards/logged-in.guard';
 import { HomeComponent } from './home/home.component';
 import { NewSongComponent } from './songs/new-song/new-song.component';
 import { EditSongbookComponent } from './songbooks/edit-songbook/edit-songbook.component';
@@ -9,13 +10,19 @@ import { RegisterComponent } from './auth/register/register.component';
 import { LoginComponent } from './auth/login/login.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent},
-    {path: 'home', component: HomeComponent},
-    {path: 'home/:songbookId', component: HomeComponent},
-    {path: 'edit-songbook/:songbookId', component: EditSongbookComponent},
-    {path: 'edit-lyrics/:songId', component: EditLyricsComponent},
-    {path: 'new-song', component: NewSongComponent},
-    {path: 'all-songbooks', component: AllSongbooksComponent},
+    {
+        path: '', // Parent route for the group
+        canActivate: [loggedInGuard], // Apply guard if necessary
+        children: [
+          { path: '', component: HomeComponent },
+          { path: 'home', component: HomeComponent },
+          { path: 'home/:songbookId', component: HomeComponent },
+          { path: 'edit-songbook/:songbookId', component: EditSongbookComponent },
+          { path: 'edit-lyrics/:songId', component: EditLyricsComponent },
+          { path: 'new-song', component: NewSongComponent },
+          { path: 'all-songbooks', component: AllSongbooksComponent },
+        ]
+    },
     {path: 'register', component: RegisterComponent},
     {path: 'login', component: LoginComponent},
     {path: '**', component: PageNotFoundComponent},
