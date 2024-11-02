1a669eab3e45a3f1a7cfbc2bd19590c82b023dd6
ogranesson <shresthaanshu555@gmail.com>
2024-11-02 11:46:20 +0100

    Update register.component.ts

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,28 +1,48 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
-import { NgForm, FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
+import { AsyncValidatorFn } from '@angular/forms';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-  constructor(private authService: AuthService, private router: Router) { }
+  registerForm: FormGroup;
 
-  onSignup(form: NgForm) {
-    const email = form.value.email;
-    const password = form.value.passwd;
-    this.authService.signup(email, password)
-      .then((res) => {
-        if (res == 'success') {
-          this.router.navigate(['login']);
-        } else {
-          alert(res);
-        }
+  constructor( private authService: AuthService, private router: Router, private fb: FormBuilder ) {
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email], [this.emailTakenValidator()]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
     });
+  }
+
+  emailTakenValidator(): AsyncValidatorFn {
+    return (control: AbstractControl) => {
+      return this.authService.checkEmailTaken(control.value).pipe(
+        map(isTaken => (isTaken ? { emailTaken: true } : null))
+      );
+    };
+  }
+
+  onSignup() {
+    if (this.registerForm.valid) {
+      const email = this.registerForm.value.email;
+      const password = this.registerForm.value.password;
+      this.authService.signup(email, password)
+        .then(res => {
+          if (res === 'success') {
+            this.router.navigate(['login']);
+          } else {
+            alert(res);
+          }
+        });
+    }
   }
 }
