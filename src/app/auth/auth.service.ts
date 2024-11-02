372e9ca6049d951316b76ec4cc76d5ae480b1076
ogranesson <shresthaanshu555@gmail.com>
2024-11-02 11:45:02 +0100

    Add isAdmin and other email checks

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from '@angular/fire/auth';
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from '@angular/fire/auth'
+import { Firestore, collection, query, where, getDocs, CollectionReference } from '@angular/fire/firestore';
+import { Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +10,8 @@ import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAut
 export class AuthService {
   token: string | null = null;
 
-  constructor(private router: Router, private auth: Auth) {
-    if (typeof window !== 'undefined' && window.localStorage) { // defining localStorage
+  constructor(private router: Router, private auth: Auth, private db: Firestore) {
+    if (typeof window !== 'undefined' && window.localStorage) {
       const storedToken = localStorage.getItem('token');
       if (storedToken) {
         this.token = storedToken;
@@ -17,34 +19,39 @@ export class AuthService {
     }
   }
 
-  login(email: string, passwd: string): Promise<boolean> {
-    return signInWithEmailAndPassword(this.auth, email, passwd)
-      .then(() => {
-        if (this.auth.currentUser) {
-          return this.auth.currentUser.getIdToken().then((token: string) => { // saving current login state
+  login(email: string, passwd: string) {
+    return signInWithEmailAndPassword(this.auth, email, passwd).then(() => {
+      return this.auth.currentUser?.getIdToken()
+        .then(
+          (token:string) => {
             this.token = token;
             localStorage.setItem('token', token);
             return true;
-          });
-        } else {
-          return false;
-        }
-      })
-      .catch(error => {
+          }
+        );
+    })
+    .catch(
+      error => {
         console.log(error);
-        return false;
-      });
+        return false
+      }
+    )
   }
 
   logout(): void {
-    this.auth.signOut();
-    this.token = null;
-    localStorage.removeItem('token');
-    this.router.navigate(['/login']);
+    this.auth.signOut()
+      .then(() => {
+        this.token = null;
+        localStorage.removeItem('token');
+        this.router.navigate(['/login']);
+      })
+      .catch(error => {
+        console.error("Logout error:", error);
+      });
   }
 
   isLoggedIn(): boolean {
-    return this.token != null;
+    return this.token !== null;
   }
 
   signup(email: string, passwd: string): Promise<string> {
@@ -55,14 +62,54 @@ export class AuthService {
       })
       .then(() => {
         return 'success';
-      });
+      })
   }
 
-  getUid() {
+  getUid(): string | null {
     if (this.auth.currentUser) {
       return this.auth.currentUser.uid;
+    } else {
+      console.error('Error fetching UID: User is not authenticated.');
+      return null;
+    }
+  }
+
+  checkEmailTaken(email: string): Observable<boolean> {
+    const usersRef = collection(this.db, 'users');
+    const q = query(usersRef, where('email', '==', email));
+    return new Observable<boolean>(observer => {
+      getDocs(q)
+        .then((snapshot: { empty: any; }) => {
+          observer.next(!snapshot.empty);
+          observer.complete();
+        })
+        .catch((error: any) => {
+          console.error("Error checking email:", error);
+          observer.next(false);
+          observer.complete();
+        });
+    });
+  }
+
+  checkEmailExists(email: string): Observable<boolean> {
+    return this.checkEmailTaken(email);
+  }
+
+  isAdmin(): Promise<boolean> { // promise needed because getDocs is an async function
+    if (!this.auth.currentUser) {
+      return Promise.resolve(false);
     }
 
-    return null;
+    const adminQuery = query(collection(this.db, 'administrators') as CollectionReference,
+                              where('__name__', '==', this.auth.currentUser.uid)); // querying the admins collection
+
+    return getDocs(adminQuery)
+      .then(snapshot => {
+        return !snapshot.empty; // returns true if the user is an admin
+      })
+      .catch(error => {
+        console.error("Error checking admin status:", error);
+        return false;
+      });
   }
 }
