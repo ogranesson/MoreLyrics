59dbd354629dcc476049912fdaaaee767506e591
ogranesson <shresthaanshu555@gmail.com>
2024-11-02 11:45:30 +0100

    Update login and register components
    Added validators and fixed layout

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,37 +1,48 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { OnInit } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
+import { AsyncValidatorFn } from '@angular/forms';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
   invalidLogin: boolean = false;
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor( private authService: AuthService, private router: Router, private fb: FormBuilder ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
 
   ngOnInit() {
     this.invalidLogin = false;
   }
 
-  onLogin(form: NgForm) {
-    const email = form.value.email;
-    const password = form.value.passwd;
-    this.authService.login(email, password)
-      .then((response) => {
-        if (!response) {
-          this.invalidLogin = true;
-        } else {
-          this.invalidLogin = false;
-          this.router.navigate(['home']);
-        }
-      });
+  onLogin() {
+    if (this.loginForm.valid) {
+      const email = this.loginForm.value.email;
+      const password = this.loginForm.value.password;
+      this.authService.login(email, password)
+        .then(response => {
+          if (!response) {
+            this.invalidLogin = true;
+          } else {
+            this.invalidLogin = false;
+            this.router.navigate(['home']);
+          }
+        });
+    } else {
+      this.invalidLogin = true;
+    }
   }
 }
